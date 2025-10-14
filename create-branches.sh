48cd01265a9fe254f2fb7a0f95f600dabaa72e0f
Marc Wäckerlin <4056726+mwaeckerlin@users.noreply.github.com>
2025-10-14 18:06:06 +0200

    script to create / rebuild branches

---
+++ create-branches.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git checkout master
+git pull
+for i in {1..32}; do
+    git checkout $i 2>/dev/null || git checkout -b $i
+    git pull
+    git reset --hard origin/master
+    sed -i 's/ENV SOURCE_FILE="latest.*"/ENV SOURCE_FILE="latest-'$i'.tar.bz2"/g' Dockerfile
+    date > rebuilt
+    git add .
+    git commit -m "Update to latest-$i"
+    git push -f origin $i
+done
